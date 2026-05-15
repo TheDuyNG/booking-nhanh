f1091b93b66783703a2588b03f46464b239c20ad
theduy <theduy>
2026-05-15 10:50:08 +0700

    implement homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import AdsBanner from '../../components/ads-banner/AdsBanner';
+import PopularThisWeek from './components/popularThisWeek/PopularThisWeek';
+import TopPartner from './components/topPartner/TopPartner';
 
 const HomePage = () => {
+
     return (
         <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the Home Page!</p>
+            <AdsBanner
+                items={["TingXi Tan", "Derek Zheng", "Gabriel Chua"]}
+                style={{ backgroundColor: "#f0f0f0", padding: "10px 0" }}
+            />
+
+            <div>
+                <PopularThisWeek />
+            </div>
+
+
+            <div className='mt-4'>
+                <TopPartner />
+            </div>
+
         </div>
     );
 };
