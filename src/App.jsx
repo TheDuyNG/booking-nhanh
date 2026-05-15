83cbf4be9eba7ac8d8c0c80378aadd7959274f78
TheDuyNG <nguyentheduy.forwork@gmail.com>
2026-05-16 04:40:27 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import HomePage from './pages/home/HomePage'
+import BookingPage from './pages/booking/BookingPage'
+import AboutPage from './pages/about/AboutPage'
+import './i18n'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-
   return (
-    <>
+    <Router>
       <Navbar />
-      <HomePage />
-      <div className='mt-52'>
-
-      </div>
-      <Footer/>
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/booking' element={<BookingPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
