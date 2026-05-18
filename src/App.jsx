1873f1a5fb01103c43f59531c2195744ffe78624
TheDuyNG <nguyentheduy.forwork@gmail.com>
2026-05-18 22:07:46 +0700

    protected router

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Navbar from './components/navbar/Navbar'
 import HomePage from './pages/home/HomePage'
 import BookingPage from './pages/booking/BookingPage'
 import AboutPage from './pages/about/AboutPage'
+import ServicesPage from './pages/services/ServicesPage'
+import Login from './pages/Login'
+import AdminPage from './pages/admin/AdminPage'
+import ProtectedRoute from './auth/ProtectedRoute'
+import Register from './pages/Register'
 import './i18n'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
@@ -13,8 +18,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/services' element={<ServicesPage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/booking' element={<BookingPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/admin/login' element={<Navigate to="/login" replace />} />
+        <Route path='/admin' element={<ProtectedRoute><AdminPage /></ProtectedRoute>} />
       </Routes>
       <Footer />
     </Router>
