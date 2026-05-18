d6e37d80aa5a9883d1924d3dcbf3af5f7f178267
TheDuyNG <nguyentheduy.forwork@gmail.com>
2026-05-18 22:09:21 +0700

    api up

---
+++ src/config/api.js
@@ -0,0 +1,5 @@
+export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3000'
+
+export function authHeaders(token) {
+  return token ? { Authorization: `Bearer ${token}` } : {}
+}
